2ef141604f582f11113935187454d0225ed31397
julienhemond <julhemond@gmail.com>
2024-11-23 00:51:39 -0500

    Contenu de la Navbar présent et début du tailwind css sur les boutons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
 import logo from './logo.svg';
+import logoDev from './logo-developper-engineering.png';
 import './App.css';
+
+
+function HeaderButton({ buttonTextContent }) {
+  return (
+    <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" >{buttonTextContent}</button>
+  );
+}
+
+function ProjectButton() {
+  return <HeaderButton buttonTextContent="Projets" />;
+}
+
+function AboutButton() {
+  return <HeaderButton buttonTextContent="À propos" />;
+}
+
+function GithubButton() {
+  return <HeaderButton buttonTextContent="Github" />;
+}
+
+function ContactButton() {
+  return <HeaderButton buttonTextContent="Contact" />;
+}
+
+
+
+// Navbar structure
+function Navbar() {
+  return (
+    <nav>
+      <img src={logoDev} alt="Logo developper and engineer" />
+      <h2>Julien Hémond</h2>
+      <div>
+        <ProjectButton />
+        <AboutButton />
+        <GithubButton />
+        <ContactButton />
+      </div>
+      {/* TODO : add envoyer un message button */}
+    </nav>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <Navbar />
       </header>
+
+
     </div>
   );
 }
