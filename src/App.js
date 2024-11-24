0def28f99ce59da9dd9701925e836ece201f72c4
julienhemond <julhemond@gmail.com>
2024-11-24 14:24:27 -0500

    App.js avec le bouton github fonctionnel et la navbar en flexbox

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import logoDev from './logo-developper-engineering.png';
 import './App.css';
 
 
-function HeaderButton({ buttonTextContent }) {
+function HeaderButton({ buttonTextContent, onClickHandler }) {
   return (
-    <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" >{buttonTextContent}</button>
+    <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" onClick={onClickHandler}>
+      {buttonTextContent}
+    </button>
   );
 }
 
 function ProjectButton() {
-  return <HeaderButton buttonTextContent="Projets" />;
+  return <HeaderButton buttonTextContent="Projets" onClick="" />;
 }
 
 function AboutButton() {
@@ -18,7 +20,10 @@ function AboutButton() {
 }
 
 function GithubButton() {
-  return <HeaderButton buttonTextContent="Github" />;
+  const visitGithub = () => {
+    window.open("https://github.com/julienhemond", "_blank");
+  }
+  return <HeaderButton buttonTextContent="Github" onClickHandler={visitGithub} />;
 }
 
 function ContactButton() {
@@ -30,10 +35,10 @@ function ContactButton() {
 // Navbar structure
 function Navbar() {
   return (
-    <nav>
-      <img src={logoDev} alt="Logo developper and engineer" />
-      <h2>Julien Hémond</h2>
-      <div>
+    <nav className="flex flex-no-wrap flex-row justify-between">
+      <div className='flex-initial'><img src={logoDev} alt="Logo developper and engineer" className="object-contain h-full" /></div>
+      <div className='flex-initial'><h2>Julien Hémond</h2></div>
+      <div className='flex-initial flex flex-no-wrap flex-row justify-end content-start'>
         <ProjectButton />
         <AboutButton />
         <GithubButton />
